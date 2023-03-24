d28205186a599354fcc70d6116354c2b90548b72
Arya Phadke <phadkearya@gmail.com>
2023-03-25 00:23:07 +0530

    generalising for n

--- flow.pl
+++ flow.pl
@@ -1,39 +1,43 @@
-setup1 :-
+setup1(N) :-
 % Normal setup process
     consult('Desktop/sem6/SSL/Assignment4/platform-ubuntu.pl'),
     start_tartarus(localhost,6001,1),
     consult('Desktop/sem6/SSL/MidSem/agent.pl'),
-    startSession(200, 6001, 3),
+    startSession(200, 6001, N),
     roundInitialiser.
 
 setup11 :-
     roundWork.
-    % sendMessage(6001,1,2345,1,6000).
-    % sendAck(6000,6001).
 
-setup2 :-
+setup2(N) :-
 % Normal setup process
     consult('Desktop/sem6/SSL/Assignment4/platform-ubuntu.pl'),
     start_tartarus(localhost,6002,1),
     consult('Desktop/sem6/SSL/MidSem/agent.pl'),
-    startSession(250, 6002, 3),
+    startSession(250, 6002, N),
     roundInitialiser.
 
 setup21 :-
     roundWork.
-    % sendMessage(6001,1,2345,1,6000).
-    % sendAck(6000,6001).
 
-setup3 :-
+setup3(N) :-
 % Normal setup process
     consult('Desktop/sem6/SSL/Assignment4/platform-ubuntu.pl'),
     start_tartarus(localhost,6003,1),
     consult('Desktop/sem6/SSL/MidSem/agent.pl'),
-    startSession(300, 6003, 3),
+    startSession(300, 6003, N),
     roundInitialiser.
 
 setup31 :- 
     roundWork.
-    % sendMessage(6001,1,2345,1,6000).
-    % sendAck(6000,6001).
 
+setup4(N) :-
+% Normal setup process
+    consult('Desktop/sem6/SSL/Assignment4/platform-ubuntu.pl'),
+    start_tartarus(localhost,6004,1),
+    consult('Desktop/sem6/SSL/MidSem/agent.pl'),
+    startSession(350, 6004, N),
+    roundInitialiser.
+
+setup41 :- 
+    roundWork.
