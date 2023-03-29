1bfbd7aeca51eaace2f634213e67071a6f3e93a6
Arya Phadke <phadkearya@gmail.com>
2023-03-29 18:54:43 +0530

    adding comments 1

--- flow.pl
+++ flow.pl
@@ -1,47 +1,15 @@
-setup1(N, Pref, P) :-
+% N is the number of processes, Pref is the starting preference of the process and P is the processID of the process.
+
+setup(N, Pref, P) :-
 % Normal setup process
     Temp is 6000 + P,
     consult('Desktop/sem6/SSL/Assignment4/platform-ubuntu.pl'),
     start_tartarus(localhost,Temp,1),
-    consult('Desktop/sem6/SSL/MidSem/agent.pl'),
+    consult('Desktop/sem6/SSL/MidSem/run.pl'),
     startSession(Pref, Temp, N).
 
-setup11 :-
+% setupNext simply executed the predicates defined in run.pl
+setupNext :-
     roundInitialiser,
     roundWork.
 
-/*setup2(N) :-
-% Normal setup process
-    consult('Desktop/sem6/SSL/Assignment4/platform-ubuntu.pl'),
-    start_tartarus(localhost,6002,1),
-    consult('Desktop/sem6/SSL/MidSem/agent.pl'),
-    startSession(250, 6002, N),
-
-setup21 :-
-    roundInitialiser,
-    roundWork.
-
-setup3(N) :-
-% Normal setup process
-    consult('Desktop/sem6/SSL/Assignment4/platform-ubuntu.pl'),
-    start_tartarus(localhost,6003,1),
-    consult('Desktop/sem6/SSL/MidSem/agent.pl'),
-    startSession(300, 6003, N),
-    roundInitialiser.
-
-setup31 :- 
-    roundInitialiser,
-    roundWork.
-
-setup4(N) :-
-% Normal setup process
-    consult('Desktop/sem6/SSL/Assignment4/platform-ubuntu.pl'),
-    start_tartarus(localhost,6004,1),
-    consult('Desktop/sem6/SSL/MidSem/agent.pl'),
-    startSession(350, 6004, N),
-    roundInitialiser.
-
-setup41 :- 
-    roundInitialiser,
-    roundWork.
-*/
